57b21f9a79f5abaf45896e0f29fffcb8578f08bd
rexoliveira <roolivei@yahoo.com.br>
2022-08-16 17:02:51 -0300

    Concluido parte1 repetições

---
+++ Modulo-E/Aula013/ex011.js
@@ -0,0 +1,21 @@
+var contador = 1
+do {
+    console.log(`Tudo bem?-${contador}`)
+    contador++
+}
+while (contador <= 6)
+
+
+/* var contador = 1
+
+while (contador <= 6) {
+    console.log(`Tudo bem?-${contador}`)
+    contador++
+} */
+
+/* console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`) */
